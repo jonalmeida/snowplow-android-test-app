9a28146d754b1af90672211e533cc7851d359202
Jonathan Almeida <jonathan.almeida942@gmail.com>
2014-09-12 11:46:21 -0400

    Added some Settings Activity stuff

---
+++ app/src/main/java/testApp/com/snowplowanalytics/SettingsFragment.java
@@ -0,0 +1,11 @@
+package testApp.com.snowplowanalytics;
+
+import android.app.Fragment;
+import android.os.Bundle;
+
+public class SettingsFragment extends Fragment {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
